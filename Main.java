7a34ca096c62612489aeb6a1be4689ac1508b9c8
kexxxon <dev.kexon@gmail.com>
2022-07-22 23:30:23 +0900

    boj_10818

---
+++ Main.java
@@ -0,0 +1,25 @@
+/* No. 10818 */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        // Array declaration & size expansion at the same time
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        // Sort elements with ascending order in Arrays
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[N - 1]);
+    }
+}
