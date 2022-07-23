f5c3e0a10341d9e374ff981bacf246319db7aa72
kexxxon <dev.kexon@gmail.com>
2022-07-23 17:17:23 +0900

    factorial

--- boj_10872.java
+++ boj_10872.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class boj_10872 {
+import java.util.Scanner;
+
+public class boj_10872 {
+    public static int factorial(int N) {
+        if(N == 0) return 1;
+
+        return N * factorial(N - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.close();
+
+        System.out.println(factorial(N));
+    }
 }
