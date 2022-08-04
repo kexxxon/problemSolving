ce5ddceac7ff8de7d741a1dce0d70878d75e3e47
kexxxon <dev.kexon@gmail.com>
2022-08-05 02:39:56 +0900

    별찍기 - 1

---
+++ controlFlowStatement/boj_2438.java
@@ -0,0 +1,20 @@
+package controlFlowStatement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
